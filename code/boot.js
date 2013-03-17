59d7673e34e7070aaf58d2a7e9b2e32761d6c947
Grischa Brockhaus <github@brockha.us>
2013-03-17 03:59:19 +0100

    Images brockhaus

--- code/boot.js
+++ code/boot.js
@@ -321,7 +321,7 @@ function boot() {
   window.runOnSmartphonesBeforeBoot();
 
   // overwrite default Leaflet Marker icon to be a neutral color
-  var base = 'https://iitcserv.appspot.com/dist/images';
+  var base = 'https://raw.github.com/brockhaus/ingress-intel-total-conversion/gh-pages/dist/images';
   L.Icon.Default.imagePath = base;
 
   window.iconEnl = L.Icon.Default.extend({options: { iconUrl: base + '/marker-green.png' } });
